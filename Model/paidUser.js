31a8cf943fc73cc4f63f4d04c403ef34b84dda29
tobias birch <tobi20ac@student.cbs.dk>
2020-10-18 18:59:00 +0200

    final commit

--- Model/paidUser.js
+++ Model/paidUser.js
@@ -5,7 +5,7 @@ const User = require("./user")
 //Importerer Creditcard
 const Creditcard = require("./creditcard")
 
-//Klasse der repræsenterer en Paid User. 
+//Klasse der repræsenterer en Paid User. Nedarver fra User. 
 class PaidUser extends User{
     constructor(firstName, lastName, username, password, birthday, description, image, interest1, interest2, cardNmb){
         super(firstName, lastName, username, password, birthday, description, image, interest1, interest2);
